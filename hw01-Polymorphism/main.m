5dda10a2f2e67de834bfac952aa034d172a09dac
Asdruval De Leon <aproasdruval@gmail.com>
2017-09-25 23:33:15 -0400

    Initial Commit

---
+++ hw01-Polymorphism/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hw01-Polymorphism
+//
+//  Created by Asdruval De Leon on 9/25/17.
+//  Copyright © 2017 ITCPremier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
